1d1747f4dfad2b330ef6794c837595126468e8e7
s-martin <saimen54@hotmail.com>
2024-04-16 20:50:01 +0200

    start UI tests with jest

---
+++ src/webapp/src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
+
+test('if the heading is correct', () => { 
+    render(<App />);
+    //const headingElement = screen.getByText(/Todo's/i);
+    const headingElement = screen.getByText('Phoniebox');
+});
